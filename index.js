74be410cca9dea6fcd984c83b6014f61be0070d6
Reese Kunz <reesekunz21@gmail.com>
2019-09-03 15:08:45 -0700

    hobbit endpoint data showing up (from training kit)

--- index.js
+++ index.js
@@ -8,14 +8,29 @@ const server = express();
 // request handler function
 server.get("/", (request, response) => {
   // produce response and send it back to client
-  response.send("Hello World");
+  response.send("Hello World!");
+});
+
+server.get("/hobbits", (request, response) => {
+  const hobbits = [
+    {
+      id: 1,
+      name: "Samwise Gamgee"
+    },
+    {
+      id: 2,
+      name: "Frodo Baggins"
+    }
+  ];
+
+  // 200 === success
+  response.status(200).json(hobbits);
+  // go to localhost:8000/hobbits and you will see the hobbits endpoint data being sent in json
 });
 
 // tell server to listen to connection on a particular port
 server.listen(8000, () => console.log("api running on port 8000"));
 
-
-// start up with 
-// npm run server 
-// or npm start if script is already there. 
-// Should now show up in localhost:8000 
+// start up with npm run server
+// or npm start if script is already there.
+// Should now show up in localhost:8000
--- projectSteps.md
+++ projectSteps.md
@@ -1,15 +1,26 @@
-# Download Dependencies
+#1. Download Dependencies
 at root of application (don't cd into data yet)
 
 npm install - downloads dependencies & creates node modules folder
 npm i express - package.json should now include express dependency 
 
-# Create index.js file 
+#2. Create index.js file 
 
 Add index.js at root of application => index.js will become the api 
 
-# Import (Require) express into index.js 
+#3. Set up index.js 
 const express = require('express');
+etc... 
+
+#4. Start up application 
+
+npm run server 
+or npm start if script is already there. 
+Should now show up in localhost:8000 
+
+
+
+
 
 
 
